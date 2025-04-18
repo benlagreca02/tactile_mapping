cf9fe15ced13dffe876a7a9fcdb169aef267102a
Benjamin LaGreca <benlagreca02@gmail.com>
2025-04-18 16:02:46 -0400

    almost there, just need to make it move WRT the tip, rather than the 'end effector'

--- scanning/scanning/scanning.py
+++ scanning/scanning/scanning.py
@@ -33,6 +33,7 @@ class ScanNode(Node):
 
         self.get_logger().info("Requesting servoing...")
         self.start_servoing()
+
         self.get_logger().info("Servoing activated, Activate interlock! Dyna-therms connected! Infra-cells up; mega-thrusters are go!")
         self.timer = self.create_timer(self.timer_period, self.timer_callback)  
 
@@ -48,7 +49,6 @@ class ScanNode(Node):
         req = Trigger.Request()
         future = self.cli.call_async(req)
         future.add_done_callback(self.servo_service_callback)
-
 
     def servo_service_callback(self, future):
         try:
@@ -76,35 +76,59 @@ class ScanNode(Node):
             # self.twist_publisher.publish(twist_msg)
             self.get_logger().info(f'Waiting for wrench and pose: {self.wrench} {self.tcp_pose}')
             return
-        
+
         # Create and publish Twist message
         twist_msg = TwistStamped()
         twist_msg.header.stamp = self.get_clock().now().to_msg()  # Current time
-        twist_msg.header.frame_id="end_effector"
+        twist_msg.header.frame_id="tip"
 
         # 1. First, try getting it to just do 0 force (?)
 
         # twist_msg.twist = self.fake_freedrive()
-        twist_msg.twist = self.apply_downward_force_and_translate()
+        # twist_msg.twist = self.apply_downward_force_and_translate()
+        twist_msg.twist = self.test_drive()
 
         # Publish the twist message
         self.twist_publisher.publish(twist_msg)
 
-    def apply_downward_force_and_translate(self):
+    def test_drive(self):
         twist_msg = Twist()
-        target_force = -4
-        forceErr = self.wrench.wrench.force.z - target_force
-        kpDown = 2.0
+        bruh = 150*math.cos(2 * math.pi * 0.125 * time.time())
+        twist_msg.angular.y = self.d(bruh)
+        self.get_logger().info(f"twistval: {bruh}")
+        return twist_msg
 
-        if abs(forceErr) > 0.5:
-            twist_msg.linear.z = self.d(kpDown * forceErr)
+    def apply_downward_force_and_translate(self):
+        fz = self.wrench.wrench.force.z 
+        fx = self.wrench.wrench.force.x 
+        tx = self.wrench.wrench.torque.x 
+        ty = self.wrench.wrench.torque.y 
+        twist_msg = Twist()
 
-        # self.get_logger().info(f'driving with kp={kp}\terr={err}\tdrive={kp*err}')
+        # has to push with at least SOME force
+        target_force = -5
+        forceErrZ = fz - target_force
+        kpDown = 0.2
 
-        # Move in X direction with constant speed
-        twist_msg.linear.x = self.d(3)
+        # If there is force error, drive downwards
+        z = 0
+        if abs(forceErrZ) > 1:
+            z = self.d(forceErrZ * kpDown)
+            twist_msg.linear.z = z
 
-        # twist_msg.linear.z = self.d(-2.0) # kp * err
+        kpTwist = 5
+        if abs(fx) > 3:
+            twist_msg.angular.y = self.d(fx*kpTwist)
+
+
+        # decrease speed in x dir if there is force in x dir
+        kpX = 0.4
+        x = self.d(1 - kpX*fx)
+        twist_msg.linear.x = x
+
+        # self.get_logger().info(f'z={z:.4f}\tx={x:.4f}\t FORCE: z: {fz:.2f} zerr:{forceErrZ}\tx: {fz:.2f}\t TORQUE: tx:{tx:.2f}\tty:{ty:.2f}')
+        self.get_logger().info(f'z={z:.4f}\tx={x:.4f}\tfz: {fz:.2f}\t')
+
         return twist_msg
 
 
